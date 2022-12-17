b40aa28dfcc892186949b9621001a7c4446e3707
Gaser98 <76227165+Gaser98@users.noreply.github.com>
2022-12-17 02:07:28 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,9 +125,9 @@ class TestAccountService(TestCase):
 
        ######################################################################
         # READ AN ACCOUNT
-        ######################################################################
-        @app.route("/accounts/<int:account_id>", methods=["GET"])
-        def get_accounts(account_id):
+     ######################################################################
+     @app.route("/accounts/<int:account_id>", methods=["GET"])
+     def get_accounts(account_id):
             """
             Reads an Account
             This endpoint will read an Account based the account_id that is requested
@@ -137,10 +137,10 @@ class TestAccountService(TestCase):
             if not account:
                 abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
             return account.serialize(), status.HTTP_200_OK  
-          def test_get_account_not_found(self):
-            """It should not Read an Account that is not found"""
-              resp = self.client.get(f"{BASE_URL}/0")
-              self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+       def test_get_account_not_found(self):
+           """It should not Read an Account that is not found"""
+            resp = self.client.get(f"{BASE_URL}/0")
+            self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
      ######################################################################
     # LIST ALL ACCOUNTS
     ######################################################################
